4cd850f89e32d1377fd67e1a9b4aa48bbae62ce5
aurelien dhellemme <dhellemme.a@gmail.com>
2020-06-17 01:20:23 +0200

    mvcdog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,46 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @dogs = Dog.all
+    end
+    
+    def new 
+        @dog = Dog.new
+    end
+    
+    def create
+        @dog = Dog.new(dog_params)
+
+        if @dog.save
+            redirect_to @dog, notice: 'Felicitations ! Ton chien a été créé !'
+        else
+            render :new
+        end
+    end
+
+    def update
+        if @dog.update(dog_params)
+          redirect_to @dog, notice: 'La update a été enregistré.'
+        else
+          render :edit
+        end
+    end
+
+    
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect _to dogs_path(@dog)
+    end
+
+    private
+
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit(:race, :location, :name, :age, :price, :description)
+    end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,13 @@
 class Dog < ApplicationRecord
     belongs_to :user
+    has_many :bookings, dependent: :destroy
+    has_many :reviews, through: :bookings
     
+    validates :race, presence :true
+    validates :location, presence :true
+    validates :user, presence :true
+    validates :name, presence :true
+    validates :age, presence :true
+    validates :price, presence :true
+    validates :description, presence :true
 end
