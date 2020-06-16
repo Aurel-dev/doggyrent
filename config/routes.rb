2022664fb444f29f8499dd562e3866a277e5861a
Maxence Heyndrickx <m.heyndrickx@hotmail.fr>
2020-06-16 15:29:45 +0200

    bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post "/bookings", to: "bookings#create"
-  delete "/bookings/:id", to: "bookings#destroy", as: :delete
   post "/bookings/:id/review", to: "reviews#create"
-  get "/bookings/:id/edit", to:"bookings#edit", as: :edit
-  get "/bookings", to: "bookings#index"
+  get "/bookings/:id/edit", to:"bookings#edit"
+  delete "/bookings/:id", to: "bookings#destroy"
 
   get "/dogs", to: "dogs#index"
-  get "/dogs/:id", to:"dogs#show", as: :show
-  get "/dogs/:id/edit", to:"dogs#edit", as: :edit
+  get "/dogs/:id", to:"dogs#show"
+  get "/dogs/:id/edit", to:"dogs#edit"
   get "/dogs/new", to: "dogs#new"
   post "/dogs", to: "dogs#create"
-  delete "/dogs/:id", to: "dogs#destroy", as: :delete
-  patch "/dogs/id:", to: "dogs#update", as: :dogs #a verif
-
-  post "/device", to: "//#update", as: :/ # a verif
+  delete "/dogs/:id", to: "dogs#destroy"
+  patch "/dogs/id:", to: "dogs#update"
 
 
 end
