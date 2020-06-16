450bc4cb5271a05ef05812f38cb4d60fc83c57e4
aurelien dhellemme <dhellemme.a@gmail.com>
2020-06-17 01:40:59 +0200

    mvcreview

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,49 @@
 class ReviewsController < ApplicationController
+
+    before_action :set_review, only: [ :show, :update, :destroy ]
+
+    def index
+        @reviews = Review.all
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+        @dog = Dog.find(params[:id])
+        @review.dog = @dog
+        if @review.save!
+          redirect_to dog_path(@dog)
+        else
+          @review = Review.new
+          render :show
+        end
+    end
+
+    def update
+        @dog = @review.booking.dog
+        if @review.update(review_params)
+          redirect_to dog_path(@dog)
+        else
+          render :show
+        end
+    end
+
+    def destroy
+        @review.destroy
+        @dog = @review.booking.dog
+        redirect_to dog_path(@dog)
+    end
+
+    private
+
+    def set_review
+        @review = Review.find(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:booking, :comment, :rating)
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :booking
+
+    validates :booking, presence: true
+    validates :comment, presence: true
+    validates :rating, presence: true
 end
