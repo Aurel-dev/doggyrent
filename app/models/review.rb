b09a544faaee56b9f8747c5c17c513812a2c566c
alexandre Bordeyne <bordeynea@gmail.com>
2020-06-17 13:32:26 +0200

    verifier le fonctionnement de la db

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,7 @@
 class Review < ApplicationRecord
-
     belongs_to :booking
-
     validates :booking, presence: true
     validates :comment, presence: true
     validates :rating, presence: true
-
-  belongs_to :booking
 
 end
